d46a9ebb37970bf6ea9147cf3d3f0614432ea517
liushitian <liushitian0604@163.com>
2022-08-21 16:08:19 +0800

    Spring AOP 环绕通知

--- course-02-spring-aop/src/main/java/com/mori/utils/Logger.java
+++ course-02-spring-aop/src/main/java/com/mori/utils/Logger.java
@@ -1,5 +1,7 @@
 package com.mori.utils;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * 【通知类】
  * 用于记录日志的工具类
@@ -34,4 +36,24 @@ public class Logger {
         System.out.println("最终通知：Logger类afterPrintLog方法开始记录日志了……");
     }
 
+    /**
+     * 环绕通知：可以通过代码确定通知何时执行
+     * <p>
+     * 正确使用方式：使用spring提供的ProceedingJoinPoint.proceed(args)接口方法，明确切入点调用。
+     */
+    public Object aroundPrintLog(ProceedingJoinPoint pjp) {
+        try {
+            System.out.println("环绕通知：前置");
+            Object[] args = pjp.getArgs(); //获取切入点方法所需参数
+            Object rtValue = pjp.proceed(args);//明确切入点调用
+            System.out.println("环绕通知：后置");
+            return rtValue;
+        } catch (Throwable t) {  //必须Throwable
+            System.out.println("环绕通知：异常");
+            t.printStackTrace();
+            throw new RuntimeException(t);
+        } finally {
+            System.out.println("环绕通知：最终");
+        }
+    }
 }
