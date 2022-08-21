d95e9c2745e32cbf9852bcfc70d6fa97ab49cc6f
liushitian <liushitian0604@163.com>
2022-08-21 19:11:15 +0800

    简化import

--- course-02-spring-ioc-anno/src/test/java/service/AccountServiceTest.java
+++ course-02-spring-ioc-anno/src/test/java/service/AccountServiceTest.java
@@ -3,13 +3,9 @@ package service;
 import com.mori.config.SpringConfiguration;
 import com.mori.domain.Account;
 import com.mori.service.AccountService;
-import com.mori.service.impl.AccountServiceImpl;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
