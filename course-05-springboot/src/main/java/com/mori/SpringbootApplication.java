f2b11ab0e8ca9479e5ea59c5cb0a3c0819a65f33
liushitian <liushitian0604@163.com>
2022-09-11 17:55:43 +0800

    SpringBoot 包扫描Mapper

--- course-05-springboot/src/main/java/com/mori/SpringbootApplication.java
+++ course-05-springboot/src/main/java/com/mori/SpringbootApplication.java
@@ -1,9 +1,11 @@
 package com.mori;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mori.mapper")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
--- course-05-springboot/src/test/java/com/mori/MybatisTest.java
+++ course-05-springboot/src/test/java/com/mori/MybatisTest.java
@@ -4,17 +4,17 @@ import com.mori.domain.User;
 import com.mori.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootApplication.class)
 public class MybatisTest {
 
-    @Resource
+    @Autowired
     private UserMapper userMapper;
 
     @Test
